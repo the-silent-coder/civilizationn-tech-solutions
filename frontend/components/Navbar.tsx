c61307a77cff3c9592f51602c1987b483b77d049
the-silent-coder <the.silent.coder.06@gmail.com>
2026-06-16 02:43:01 +0530

    Create homepage architecture

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <h2>Civilizationn Tech Solutions</h2>
+        </nav>
+    );
+}
---
+++ frontend/sections/HeroSection.tsx
@@ -0,0 +1,12 @@
+export default function HeroSection() {
+    return (
+        <section>
+            <h1>Civilizationn Tech Solutions</h1>
+
+            <p>
+                Any Tech Solutions You Need,
+                We Have Your Back.
+            </p>
+        </section>
+    );
+}
